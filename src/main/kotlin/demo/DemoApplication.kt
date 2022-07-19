aaccd9860db479f5559b4283a4b2d2c94e81e597
timo-taling <timo@taling.me>
2022-07-19 20:58:26 +0900

    Create a data class and a controller

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -2,6 +2,8 @@ package demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
@@ -9,3 +11,15 @@ class DemoApplication
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Bonjour!"),
+		Message("3", "Privet!"),
+	)
+}
+
+data class Message(val id: String?, val text: String)
